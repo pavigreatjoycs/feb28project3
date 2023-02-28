71965ded3c12eb406a9805b10d7329f5bb751c48
Great Joycs <pavigreatjoycs@gmail.com>
2023-02-28 18:17:51 +0530

    Added second line to HelloWorld

--- feb28project3/src/main/java/com/luv2code/pkg/HelloWorld.java
+++ feb28project3/src/main/java/com/luv2code/pkg/HelloWorld.java
@@ -3,6 +3,7 @@ package main.java.com.luv2code.pkg;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("MY New Hello World");
 	}
 
 }
