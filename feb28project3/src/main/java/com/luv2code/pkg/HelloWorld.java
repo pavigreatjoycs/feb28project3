3f087204766044f0a8a06d543ffbb661e94cde82
Great Joycs <pavigreatjoycs@gmail.com>
2023-02-28 18:25:52 +0530

    Added helloworld and gitignore

--- feb28project3/src/main/java/com/luv2code/pkg/HelloWorld.java
+++ feb28project3/src/main/java/com/luv2code/pkg/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("MY New Hello World");
+		System.out.println("traffic from pc to github");
 	}
 
 }
